9dcfaa39757627237b28cb46a6417602cace3e90
Horos20 <80223594+Horos20@users.noreply.github.com>
2021-12-08 11:09:50 +0200

    Initial Commit 2

---
+++ Player/Player.gd
@@ -0,0 +1,14 @@
+extends KinematicBody2D
+
+var velocity = Vector2.ZERO
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	
+	if input_vector != Vector2.ZERO:
+		velocity = input_vector
+	else:
+		velocity = Vector2.ZERO
+	
+	move_and_collide(velocity)
