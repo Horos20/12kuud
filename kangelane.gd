947b281145931d7bcc2b908d262c17aa0fc5c376
JurgenMik <89903354+JurgenMik@users.noreply.github.com>
2022-01-06 02:44:16 +0200

    sügis/gravity/movement/background/spike

---
+++ kangelane.gd
@@ -0,0 +1,36 @@
+extends KinematicBody2D
+
+const UP = Vector2(0, -1)
+const GRAVITY = 20
+const MAXFALLSPEED = 250
+const MAXSPEED = 100
+const JUMPFORCE = 370
+
+
+
+
+
+var motion = Vector2()
+
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	
+	motion.y += GRAVITY
+	if motion.y > MAXFALLSPEED:
+		motion.y = MAXFALLSPEED
+	
+	if Input.is_action_pressed("right"):
+		motion.x = MAXSPEED
+	elif Input.is_action_pressed("left"):
+		motion.x = -MAXSPEED
+	else:
+		motion.x = 0
+		
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			motion.y = -JUMPFORCE
+		
+	motion = move_and_slide(motion, UP)
+	
